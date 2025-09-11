9c0b7e6ca84e7ac99ab36103717f37f8d61647cf
harlock <xarlokua@gmail.com>
2025-09-11 20:58:23 +0300

    les4 done

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
 
 Route::get('/second', function () {
     return view('second');
 });
+?>
