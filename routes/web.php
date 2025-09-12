5ae340d295ceed3f874065be376fc4dece3beb87
harlock <xarlokua@gmail.com>
2025-09-12 23:13:19 +0300

    article page lesson5

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+Route::view('article', 'article')->name('article');
 
 Route::get('/second', function () {
     return view('second');
