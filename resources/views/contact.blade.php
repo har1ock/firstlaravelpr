437291b0f9bc5fdb201bff99c15c969e3ed11ab4
harlock <xarlokua@gmail.com>
2025-09-14 23:11:08 +0300

    .

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 class="text-3xl font-bold">Contact Us</h2>
+    <h2 class="text-3xl font-bold">Contact Us!</h2>
     <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
 
 @endsection
