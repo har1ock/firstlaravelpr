72154a46e38fb6c1e72be7a9d84c19641be0644b
harlock <xarlokua@gmail.com>
2025-09-15 23:09:40 +0300

    lesson 5 compl

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h2 class="text-3xl font-bold">About Us</h2>
-    <p class="mt-4, text-lg text-gray-600">We're the best company in the world!</p>
-
+    <main class="container mx-auto mt-6 flex justify-center">
+        <section class="w-3/5 bg-white p-6 shadow-md rounded-lg">
+            <h2 class="text-2xl font-bold mb-4">About Us</h2>
+            <p class="mt-4, text-lg text-gray-600">We're the best company in the world!</p>
+        </section>
+    </main>
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 class="text-3xl font-bold">Contact Us!</h2>
-    <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
-
+    <main class="container mx-auto mt-6 flex justify-center">
+            <section class="w-3/5 bg-white p-6 shadow-md rounded-lg">
+                <h2 class="text-3xl font-bold">Contact Us!</h2>
+                <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+            </section>
+    </main>
 @endsection
